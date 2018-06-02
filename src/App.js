76725cfc549332b6a25f9a3ebaaf14e06b1e9bff
mayur <mayursinghal383@gmail.com>
2018-06-02 17:34:05 +0530

    authorised

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class App extends Component {
+    render()
+    {
+    return (
+        <View>
+            <Text> AN APp </Text>
+            </View>
+    );
+}
+}
+
+export default App;
