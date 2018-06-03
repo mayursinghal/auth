3c55ab978c228f2a1f4ad0d2223e83de20154f47
mayur <mayursinghal383@gmail.com>
2018-06-03 12:24:04 +0530

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyD6XckCmy2HXz9Shq_Jfv84gV60DBfKg44',
+            authDomain: 'auth-84b2a.firebaseapp.com',
+            databaseURL: 'https://auth-84b2a.firebaseio.com',
+            projectId: 'auth-84b2a',
+            storageBucket: 'auth-84b2a.appspot.com',
+            messagingSenderId: '380619567178'
+          });
+    }
     render() {
     return (
         
