2bef9713bd03be30d8b12234fcb378158574c0f3
mayur <mayursinghal383@gmail.com>
2018-06-05 17:11:06 +0530

    spinner

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-    state={ email: '', password: '', error: '' };
+    state={ email: '', password: '', error: '', loading: false };
 
     onButtonPress() {
         const { email, password } = this.state;
-        this.setState({ error: '' });
+        this.setState({ error: '', loading: true });
         firebase.auth().signInWithEmailAndPassword(email, password)
         .catch(() => {
             firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -16,6 +16,18 @@ class LoginForm extends Component {
                 this.setState({ error: 'Authentication Failed !!' });
             });
         });
+}
+
+renderButton() {
+    if (this.state.loading) {
+        return <Spinner size="small" />;
+    }
+
+    return (
+        <Button onPress={this.onButtonPress.bind(this)}>
+        Log In
+        </Button>
+    );
 }
 
     render() {
@@ -43,9 +55,7 @@ class LoginForm extends Component {
     {this.state.error}
         </Text>
         <CardSection> 
-            <Button onPress={this.onButtonPress.bind(this)}>
-                Log In
-                </Button>
+           { this.renderButton() }
             </CardSection>
             </Card>
         );
---
+++ src/components/common/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, ActivityIndicator } from 'react-native';
+
+const Spinner = () => (
+        <View >
+    <ActivityIndicator />
+            </View>
+    );
+
+export { Spinner };
